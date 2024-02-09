70d4f6a8b20329da94d947dc3660c2872b1c5a2c
Victoria Ayala <139914452+vimayala@users.noreply.github.com>
2024-02-09 14:46:12 -0800

    me when i cloned instead of downloading (adding folders)

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// Code Practice: Scrolling States
+// Name: Victoria Ayala
+// Date: 2/9
+
+'use strict'
+
+const config = {
+    parent: 'phaser-game',  // for info text
+    type: Phaser.WEBGL,     // for tinting
+    width: 400,
+    height: 300,
+    pixelArt: true,
+    zoom: 2,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [ Load, Play ]
+}
+
+const game = new Phaser.Game(config)
